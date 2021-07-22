637a480e7cf1ae0b2babdf643436c2ead675a784
wys <493715706@qq.com>
2021-07-22 17:47:00 +0800

    css 修复

--- es/collect-toast/collect-toast.js
+++ es/collect-toast/collect-toast.js
@@ -3,9 +3,11 @@ Component({
   mixins: [],
   data: {
     visiable: false,
-    iconUrl: 'https://alipay-data.oss-cn-hangzhou.aliyuncs.com/xcx/common/imgs/image-start.png'
+    iconUrl: 'https://alipay-data.oss-cn-hangzhou.aliyuncs.com/xcx/common/imgs/image-start.png',
+    titleBarH: 20
   },
   props: {
+    isHideTitleBar: false
     // onClose: () => { } 
   },
   didMount() {
@@ -16,6 +18,21 @@ Component({
     //     this.cancelPop();
     //   }
     // });
+    if (this.props.isHideTitleBar) {
+      my.getSystemInfo({
+        success: (res) => {
+          console.log('getSystemInfo', res);
+          this.setData({
+            titleBarH: (res.titleBarHeight + res.statusBarHeight) * 2 + 20
+          })
+        }, fail: () => {
+          this.setData({
+            titleBarH: 20
+          })
+        }
+      })
+    }
+
     my && my.isCollected({
       success: (e) => {
         console.log('', e.isCollected);
